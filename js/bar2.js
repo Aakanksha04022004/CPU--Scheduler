4fbc02feaf15a63ab65e05f1314565f5595f12d0
Aakanksha04022004 <146117000+Aakanksha04022004@users.noreply.github.com>
2024-06-17 23:39:49 +0530

    Add files via upload

---
+++ js/bar2.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    var ctx= $("#bar-chartcanvas");
+
+    var data ={
+        labels : ["testcase 1", "testcase 2", "testcase 3"],
+        datasets : [
+            {
+                label : "Windows 7",
+                data : [156,141,219],
+                backgroundColor : [
+                    "rgba(10, 20, 30, 0.3)",
+                    "rgba(10, 20, 30, 0.3)",
+                    "rgba(10, 20, 30, 0.3)"
+                ],
+                borderColor : [
+                    "rgba(10, 20, 30, 1)",
+                    "rgba(10, 20, 30, 1)",
+                    "rgba(10, 20, 30, 1)"
+                ],
+                borderWidth : 1
+            },
+            {
+                label : "Windows 10",
+                data : [32,32,47],
+                backgroundColor : [
+                    "rgba(50, 150, 250, 0.3)",
+                    "rgba(50, 150, 250, 0.3)",
+                    "rgba(50, 150, 250, 0.3)"
+                ],
+                borderColor : [
+                    "rgba(50, 150, 250, 1)",
+                    "rgba(50, 150, 250, 1)",
+                    "rgba(50, 150, 250, 1)"
+                ],
+                borderWidth : 1
+            },
+            {
+                label : "Kali Linux",
+                data : [28,19,15],
+                backgroundColor : [
+                    "rgba(10, 20, 250, 0.3)",
+                    "rgba(10, 20, 250, 0.3)",
+                    "rgba(10, 20, 250, 0.3)"
+                ],
+                borderColor : [
+                    "rgba(10, 20, 250, 1)",
+                    "rgba(10, 20, 250, 1)",
+                    "rgba(10, 20, 250, 1)"
+                ],
+                borderWidth : 1
+            }
+        ]
+
+    };
+
+    var options = {
+        title : {
+            display : true,
+			position : "top",
+			text : "Bar Graph showing comparision between excution times taken by different OS",
+			fontSize : 18,
+			fontColor : "#111"
+        },
+        legend : {
+			display : true,
+			position : "bottom"
+        },
+        scales : {
+			yAxes : [{
+				ticks : {
+					min : 0
+				}
+			}]
+		}
+    };
+
+    var chart = new Chart(ctx , {
+        type : "bar",
+        data : data,
+        options : options
+    });
+});
